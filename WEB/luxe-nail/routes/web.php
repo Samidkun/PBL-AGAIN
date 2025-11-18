053f48e1259c72f70864842385537705c3e08492
Fakhira zahrany <fakhirazahra88@gmail.com>
2025-11-18 20:09:26 +0700

    update route dan sidebar untuk kategori

--- WEB/luxe-nail/routes/web.php
+++ WEB/luxe-nail/routes/web.php
@@ -4,11 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ReservationController;
-use App\Http\Controllers\Api\AuthController; // cukup sekali
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\OwnerReservationController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\IncomeController;
 use App\Http\Controllers\StaffController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TreatmentTypeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -70,3 +72,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/reservations/{id}', [ReservationController::class, 'getReservation']);
     Route::put('/reservations/{id}', [ReservationController::class, 'updateReservation']);
 });
+
+// ====== CATEGORY MANAGEMENT ======
+Route::get('/kategori', [CategoryController::class, 'index'])->name('kategori.index');
+Route::get('/kategori/create', [CategoryController::class, 'create'])->name('kategori.create');
+Route::post('/kategori', [CategoryController::class, 'store'])->name('kategori.store');
+Route::get('/kategori/{category}/edit', [CategoryController::class, 'edit'])->name('kategori.edit');
+Route::put('/kategori/{category}', [CategoryController::class, 'update'])->name('kategori.update');
+Route::delete('/kategori/{category}', [CategoryController::class, 'destroy'])->name('kategori.destroy');
+Route::get('/kategori/{category}/ajax-edit', [CategoryController::class, 'ajaxEdit'])->name('kategori.ajax-edit');
+Route::post('/kategori/ajax-store', [CategoryController::class, 'ajaxStore'])->name('kategori.ajax-store');
+Route::put('/kategori/{category}/ajax-update', [CategoryController::class, 'ajaxUpdate'])->name('kategori.ajax-update');
+Route::get('/kategori/get-create-form', [CategoryController::class, 'getCreateForm'])->name('kategori.get-create-form');
+Route::delete('/kategori/{category}/ajax-delete', [CategoryController::class, 'ajaxDestroy'])->name('kategori.ajax-destroy');
+
+// TREATMENT TYPES
+Route::resource('treatment-types', TreatmentTypeController::class);
